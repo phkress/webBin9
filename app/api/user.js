933cb6fff56abe17eddfb959cc878e9b099ab62b
Pedro Kress <phkress@gmail.com>
2019-01-31 15:38:15 -0200

    update api/route user

--- app/api/user.js
+++ app/api/user.js
@@ -2,9 +2,9 @@ const db = require('../../config/database');
 const api = {};
 
 api.lista = function(req, res) {
-  db.adm.find({}).exec(function(err, user) {
+  db.adm.find({}).exec(function(err, users) {
     if (err) return console.log(err);
-    res.json(user);
+    res.json(users);
   });
 };
 api.remove = (req,res) => {
@@ -18,5 +18,23 @@ api.remove = (req,res) => {
         }
     });
 };
-
+api.atualiza = (req,res) => {
+  db.adm.update({ _id: req.params.userId }, req.body, function(err, userUpdate) {
+    if (err) return console.log(err);
+    if(userUpdate) {
+      res.status(200).end();
+      console.log('Atualizado com sucesso: ' + req.body._id);
+    }else{
+      res.status(500).end();
+    }
+  });
+};
+api.adiciona = function(req, res) {
+    db.adm.insert(req.body, function(err, newUser) {
+        if(err) return console.log(err);
+        if(!newUser) return res.sendStatus(500).end();
+        console.log('Adicionado com sucesso: ' + newUser._id);
+        res.json(newHost._id);
+    });
+};
 module.exports = api;
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,11 +3,9 @@ module.exports = function(app) {
  var api = app.api.user;
 
      app.route('/z1/user')
-        .get(api.lista);
-    //     .post(api.adiciona)
-    //     ;
-    //
+        .get(api.lista)
+         .post(api.adiciona);
      app.route('/z1/user/:userId')
         .delete(api.remove)
-    //     .put(api.atualiza);
+         .put(api.atualiza);
 };
